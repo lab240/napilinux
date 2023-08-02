321b50314b7fe7c36265d4e92afcaba4a336745a
IgorKha <hahabahov@yandex.ru>
2023-08-02 20:51:21 +0500

    impr: download - add React download component - change download page md -> js

---
+++ src/components/download/index.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+
+function FileList() {
+  const sha256sumUrl = "https://packages.nnz-ipc.net/napi/nnz-napi-image/napi-rk3308b-s/SHA256SUM";
+  
+  const [imgFileList, setImgFileList] = useState([]);
+  const [swuFileList, setSwuFileList] = useState([]);
+  
+  useEffect(() => {
+    async function fetchAndProcessSHA256SUM() {
+      try {
+        const response = await fetch(sha256sumUrl);
+        const text = await response.text();
+        
+        const lines = text.trim().split("\n");
+        
+        const imgFiles = [];
+        const swuFiles = [];
+        
+        lines.forEach(line => {
+          const [sha256sum, filename] = line.split(/\s+/);
+          const fileUrl = `https://packages.nnz-ipc.net/napi/nnz-napi-image/napi-rk3308b-s/${filename}`;
+          
+          if (filename.includes(".img.")) {
+            imgFiles.push(<li key={filename}><a href={fileUrl} target="_blank">{filename}</a> - <span className="hash">{sha256sum}</span></li>);
+          }
+          
+          if (filename.includes(".swu.")) {
+            swuFiles.push(<li key={filename}><a href={fileUrl} target="_blank">{filename}</a> - <span className="hash">{sha256sum}</span></li>);
+          }
+        });
+        imgFiles.reverse();
+        swuFiles.reverse();
+        setImgFileList(imgFiles.slice(0, 2));
+        setSwuFileList(swuFiles.slice(0, 2));
+      } catch (error) {
+        console.error("Ошибка при загрузке списка:", error);
+      }
+    }
+    
+    fetchAndProcessSHA256SUM();
+  }, []);
+  
+  return (
+    <div className="container">
+    <p style={{margin: "20px 10px",}}><a className="button button--primary" href="https://packages.nnz-ipc.net/napi" target="_blank">Скачать другие ревизии</a></p>
+      <h2>Список установочных образов</h2>
+      <ul>{imgFileList}</ul>
+      <h2>Список обновлений</h2>
+      <ul>{swuFileList}</ul>
+    </div>
+  );
+}
+
+export default FileList;
---
+++ src/pages/download.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import FileList from '../../src/components/download/index';
+
+function Home() {
+  return (
+    <Layout>
+      <main>
+        <FileList />
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
