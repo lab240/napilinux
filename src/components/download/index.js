5556678fe7d27fc2c98bbc939b6c25e00d147a1f
dmnovikov <dj.novikov@gmail.com>
2024-08-16 14:27:25 +0300

    - new download site

--- src/components/download/index.js
+++ src/components/download/index.js
@@ -2,29 +2,29 @@ import React, { useEffect, useState } from 'react';
 
 function FileList() {
   const sha256sumUrl = "https://packages.nnz-ipc.net/napilinux/nnz-napi-image/napi-rk3308b-s/SHA256SUM";
-  
+
   const [imgFileList, setImgFileList] = useState([]);
   const [swuFileList, setSwuFileList] = useState([]);
-  
+
   useEffect(() => {
     async function fetchAndProcessSHA256SUM() {
       try {
         const response = await fetch(sha256sumUrl);
         const text = await response.text();
-        
+
         const lines = text.trim().split("\n");
-        
+
         const imgFiles = [];
         const swuFiles = [];
-        
+
         lines.forEach(line => {
           const [sha256sum, filename] = line.split(/\s+/);
           const fileUrl = `https://packages.nnz-ipc.net/napilinux/nnz-napi-image/napi-rk3308b-s/${filename}`;
-          
+
           if (filename.includes(".img.")) {
             imgFiles.push(<li key={filename}><a href={fileUrl} target="_blank">{filename}</a> - <span className="hash">{sha256sum}</span></li>);
           }
-          
+
           if (filename.includes(".swu.")) {
             swuFiles.push(<li key={filename}><a href={fileUrl} target="_blank">{filename}</a> - <span className="hash">{sha256sum}</span></li>);
           }
@@ -37,10 +37,10 @@ function FileList() {
         console.error("Ошибка при загрузке списка:", error);
       }
     }
-    
+
     fetchAndProcessSHA256SUM();
   }, []);
-  
+
   return (
     <div className="container">
       <p></p>
@@ -50,10 +50,10 @@ function FileList() {
     <li>Файл xxx-.swu - апдейт</li>
     </ul>
     </p>
-    <p style={{margin: "20px 10px",}}><a className="button button--primary" href="https://packages.nnz-ipc.net/napilinux/napilinux-napi-based-devs-rk3308bs/" target="_blank">NapiLinux для устройств на базе rk3308 (Napi C/P/S)</a></p>
-    <p style={{margin: "20px 10px",}}><a className="button button--primary" href="https://packages.nnz-ipc.net/napilinux/napilinux-roc-rk3328-cc/" target="_blank">Napilinux для ROC-rk3328-CC на базе rk3328</a></p>
-    <p style={{margin: "20px 10px",}}><a className="button button--primary" href="https://packages.nnz-ipc.net/napilinux/napilinux-rockpro64-rk3399/" target="_blank">Napilinux для RockPro 64 на базе rk3399(Pine64)</a></p>
-    <a href="https://packages.nnz-ipc.net/napilinux/armbian-napi-based-devs-rk3308bs/">ArmBian for Napi devices</a>
+    <p style={{margin: "20px 10px",}}><a className="button button--primary" href="https://download.napilinux.ru/linuximg/napilinux/napilinux-napi-devs-rk3308bs/" target="_blank">NapiLinux для устройств на базе rk3308 (Napi C/P/S)</a></p>
+    <p style={{margin: "20px 10px",}}><a className="button button--primary" href="https://download.napilinux.ru/linuximg/napilinux/napilinux-roc-rk3328-cc/" target="_blank">Napilinux для ROC-rk3328-CC на базе rk3328</a></p>
+    <p style={{margin: "20px 10px",}}><a className="button button--primary" href="https://download.napilinux.ru/linuximg/napilinux/napilinux-rockpro64-rk3399/" target="_blank">Napilinux для RockPro 64 на базе rk3399(Pine64)</a></p>
+    <a href="https://download.napilinux.ru/linuximg/armbian/">ArmBian for Napi devices</a>
     </div>
   );
 }
